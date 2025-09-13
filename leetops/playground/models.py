13056612acf12c7cd6ac9155e7582d0460d61c90
Tom Zhang <tommyawesome08@gmail.com>
2025-09-13 07:36:46 -0400

    Initial migrations

--- leetops/playground/models.py
+++ leetops/playground/models.py
@@ -6,3 +6,17 @@ class Rating(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     rating = models.PositiveIntegerField(default=100)
     # additional analytics/ratings can be added here in the future
+
+
+class Simulation(models.Model):
+    company = models.CharField(max_length=255)
+    company_avatar = models.ImageField(upload_to="company_avatars/")
+    details = models.TextField()
+
+
+
+class CompletedSimulation(models.Model):
+    simulation = models.ForeignKey(Simulation, on_delete=models.PROTECT)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    plus_minus = models.IntegerField() # in rating
+    summary = models.TextField()
