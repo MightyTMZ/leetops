714f5856addfbc3c82c70bf96549c02861168c09
Tom Zhang <tommyawesome08@gmail.com>
2025-09-13 07:29:14 -0400

    Comment on "Rating" model class

--- leetops/playground/models.py
+++ leetops/playground/models.py
@@ -5,3 +5,4 @@ from django.conf import settings
 class Rating(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     rating = models.PositiveIntegerField(default=100)
+    # additional analytics/ratings can be added here in the future
